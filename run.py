a6046f1b1bd3f5aa5e52fdb0ca17b04094ee8557
Terry Jia <terryjia88@gmail.com>
2024-06-22 22:59:37 -0400

    some improvement

--- run.py
+++ run.py
@@ -7,6 +7,20 @@ comfy_path = os.path.dirname(folder_paths.__file__)
 
 unique3d_path = f'{comfy_path}/custom_nodes/ComfyUI-Unique3D'
 
+output_path = f'{comfy_path}/output/Unique3D/'
+
+os.makedirs(output_path, exist_ok=True)
+
+yaml_path = os.path.join(unique3d_path, 'app', 'custom_models', 'image2mvimage.yaml')
+
+with open(yaml_path, 'r', encoding='utf-8') as file:
+    content = file.read()
+
+content = content.replace('#YOUR_COMFYUI_PATH#', unique3d_path.replace(os.path.sep, '/'))
+
+with open(yaml_path, 'w', encoding='utf-8') as file:
+    file.write(content)
+
 sys.path.append(unique3d_path)
 
 parent_dir = os.path.dirname(comfy_path)
@@ -14,17 +28,8 @@ python_embeded_path = os.path.join(parent_dir, 'python_embeded', 'Scripts')
 
 os.environ['PATH'] = os.environ['PATH'] + ";" + python_embeded_path
 
-from .app.all_models import model_zoo
 from PIL import Image
 from pytorch3d.structures import Meshes
-from .app.utils import clean_up
-from .app.custom_models.mvimg_prediction import run_mvprediction
-from .app.custom_models.normal_prediction import predict_normals
-from .scripts.refine_lr_to_sr import run_sr_fast
-from .scripts.utils import save_glb_and_video
-from .scripts.multiview_inference import geo_reconstruct
-from .scripts.sd_model_zoo import load_common_sd15_pipe
-from diffusers import StableDiffusionControlNetImg2ImgPipeline
 
 import numpy as np
 
@@ -52,15 +57,16 @@ class Unique3DLoadPipeline:
     def run(self, ip_adapter, plus_model):
         base_model = "runwayml/stable-diffusion-v1-5"
 
+        from .scripts.sd_model_zoo import load_common_sd15_pipe
+
         pipe = load_common_sd15_pipe(
             base_model=base_model, ip_adapter=ip_adapter, plus_model=plus_model,
-            controlnet=f"{unique3d_path}/ckpt/controlnet-tile",
-            pipeline_class=StableDiffusionControlNetImg2ImgPipeline)
+            controlnet=f"{unique3d_path}/ckpt/controlnet-tile")
 
         return (pipe,)
 
 
-class Unique3DRun:
+class Unique3DRunMVPrediction:
     def __init__(self):
         pass
 
@@ -69,8 +75,48 @@ class Unique3DRun:
         return {
             "required": {
                 "images": ("IMAGE",),
-                "pipe": ("Unique3DPipeline",),
                 "input_processing": ([True, False],),
+            },
+        }
+
+    RETURN_TYPES = ("PILS", "PIL",)
+    RETURN_NAMES = ("rgb_pils", "front_pil",)
+
+    FUNCTION = "run"
+
+    CATEGORY = "Unique3D"
+
+    def run(self, images, input_processing):
+        img_batch_np = images.cpu().detach().numpy().__mul__(255.).astype(np.uint8)
+
+        preview_img = Image.fromarray(img_batch_np[0])
+
+        if preview_img.size[0] <= 512:
+            from .scripts.refine_lr_to_sr import run_sr_fast
+
+            preview_img = run_sr_fast([preview_img])[0]
+
+        seed = -1
+
+        from .app.custom_models.mvimg_prediction import run_mvprediction
+
+        rgb_pils, front_pil = run_mvprediction(preview_img, input_processing, int(seed))
+
+        return (rgb_pils, front_pil,)
+
+
+class Unique3DRunGeoReconstruct:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "rgb_pils": ("PILS",),
+                "front_pil": ("PIL",),
+                "pipe": ("Unique3DPipeline",),
                 "do_refine": ([True, False],),
                 "render_video": ([True, False],),
                 "predict_normal": ([True, False],),
@@ -78,25 +124,17 @@ class Unique3DRun:
             },
         }
 
-    RETURN_TYPES = ("IMAGE","STRING",)
-    RETURN_NAMES = ("images","mesh_path",)
+    RETURN_TYPES = ("IMAGE", "STRING",)
+    RETURN_NAMES = ("images", "mesh_path",)
 
     FUNCTION = "run"
 
     CATEGORY = "Unique3D"
 
-    def run(self, images, pipe, input_processing, do_refine, render_video, predict_normal, init_type):
-        img_batch_np = images.cpu().detach().numpy().__mul__(255.).astype(np.uint8)
+    def run(self, images, rgb_pils, front_pil, pipe, do_refine, render_video, predict_normal, init_type):
+        from .scripts.multiview_inference import geo_reconstruct
 
-        preview_img = Image.fromarray(img_batch_np[0])
-
-        if preview_img.size[0] <= 512:
-            preview_img = run_sr_fast([preview_img])[0]
-
-        seed = -1
         expansion_weight = 0.1
-
-        rgb_pils, front_pil = run_mvprediction(preview_img, input_processing, int(seed))
 
         new_meshes, img_list, rm_normals = geo_reconstruct(
             pipe, rgb_pils, None, front_pil, do_refine=do_refine, predict_normal=predict_normal,
@@ -107,23 +145,25 @@ class Unique3DRun:
         vertices[..., [0, 2]] = - vertices[..., [0, 2]]
         new_meshes = Meshes(verts=[vertices], faces=new_meshes.faces_list(), textures=new_meshes.textures)
 
-        output_folder = 'tmp/gradio'
+        output_prefix = f'{output_path}/generated'
+
+        from .scripts.utils import save_glb_and_video
 
         ret_mesh, video = save_glb_and_video(
-            "/tmp/gradio/generated", new_meshes, with_timestamp=True, dist=3.5,
+            output_prefix, new_meshes, with_timestamp=True, dist=3.5,
             fov_in_degrees=2 / 1.35, cam_type="ortho", export_video=render_video)
 
-
-
-        return (images,ret_mesh,)
+        return images, ret_mesh,
 
 
 NODE_CLASS_MAPPINGS = {
-    "Unique3DRun": Unique3DRun,
+    "Unique3DRun - MVPrediction": Unique3DRunMVPrediction,
+    "Unique3DRun - Geo Reconstruct": Unique3DRunGeoReconstruct,
     "Unique3DLoadPipeline": Unique3DLoadPipeline
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Unique3DRun": Unique3DRun,
+    "Unique3DRun - MVPrediction": Unique3DRunMVPrediction,
+    "Unique3DRun - Geo Reconstruct": Unique3DRunGeoReconstruct,
     "Unique3DLoadPipeline": Unique3DLoadPipeline
 }
--- scripts/sd_model_zoo.py
+++ scripts/sd_model_zoo.py
@@ -1,4 +1,5 @@
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, EulerAncestralDiscreteScheduler, StableDiffusionControlNetImg2ImgPipeline, StableDiffusionControlNetImg2ImgPipeline, StableDiffusionPipeline
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, EulerAncestralDiscreteScheduler, \
+    StableDiffusionControlNetImg2ImgPipeline, StableDiffusionControlNetImg2ImgPipeline, StableDiffusionPipeline
 from transformers import CLIPVisionModelWithProjection
 import torch
 from copy import deepcopy
@@ -7,6 +8,7 @@ ENABLE_CPU_CACHE = False
 DEFAULT_BASE_MODEL = "runwayml/stable-diffusion-v1-5"
 
 cached_models = {}  # cache for models to avoid repeated loading, key is model name
+
 def cache_model(func):
     def wrapper(*args, **kwargs):
         if ENABLE_CPU_CACHE:
@@ -16,7 +18,9 @@ def cache_model(func):
             return cached_models[model_name]
         else:
             return func(*args, **kwargs)
+
     return wrapper
+
 
 def copied_cache_model(func):
     def wrapper(*args, **kwargs):
@@ -27,20 +31,25 @@ def copied_cache_model(func):
             return deepcopy(cached_models[model_name])
         else:
             return func(*args, **kwargs)
+
     return wrapper
 
-def model_from_ckpt_or_pretrained(ckpt_or_pretrained, model_cls, original_config_file='ckpt/v1-inference.yaml', torch_dtype=torch.float16, **kwargs):
+
+def model_from_ckpt_or_pretrained(ckpt_or_pretrained, model_cls, original_config_file='ckpt/v1-inference.yaml',
+                                  torch_dtype=torch.float16, **kwargs):
     if ckpt_or_pretrained.endswith(".safetensors"):
-        pipe = model_cls.from_single_file(ckpt_or_pretrained, original_config_file=original_config_file, torch_dtype=torch_dtype, **kwargs)
+        pipe = model_cls.from_single_file(ckpt_or_pretrained, original_config_file=original_config_file,
+                                          torch_dtype=torch_dtype, **kwargs)
     else:
         pipe = model_cls.from_pretrained(ckpt_or_pretrained, torch_dtype=torch_dtype, **kwargs)
     return pipe
+
 
 @copied_cache_model
 def load_base_model_components(base_model=DEFAULT_BASE_MODEL, torch_dtype=torch.float16):
     model_kwargs = dict(
         torch_dtype=torch_dtype,
-        requires_safety_checker=False, 
+        requires_safety_checker=False,
         safety_checker=None,
     )
     pipe: StableDiffusionPipeline = model_from_ckpt_or_pretrained(
@@ -51,10 +60,12 @@ def load_base_model_components(base_model=DEFAULT_BASE_MODEL, torch_dtype=torch.
     pipe.to("cpu")
     return pipe.components
 
+
 @cache_model
 def load_controlnet(controlnet_path, torch_dtype=torch.float16):
     controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch_dtype)
     return controlnet
+
 
 @cache_model
 def load_image_encoder():
@@ -65,30 +76,34 @@ def load_image_encoder():
     )
     return image_encoder
 
-def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controlnet=None, ip_adapter=False, plus_model=True, torch_dtype=torch.float16, model_cpu_offload_seq=None, enable_sequential_cpu_offload=False, vae_slicing=False, pipeline_class=None, **kwargs):
+
+def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controlnet=None, ip_adapter=False,
+                          plus_model=True, torch_dtype=torch.float16, model_cpu_offload_seq=None,
+                          enable_sequential_cpu_offload=False, vae_slicing=False,
+                          pipeline_class=StableDiffusionControlNetImg2ImgPipeline, **kwargs):
     model_kwargs = dict(
-        torch_dtype=torch_dtype, 
+        torch_dtype=torch_dtype,
         device_map=device,
-        requires_safety_checker=False, 
+        requires_safety_checker=False,
         safety_checker=None,
     )
     components = load_base_model_components(base_model=base_model, torch_dtype=torch_dtype)
     model_kwargs.update(components)
     model_kwargs.update(kwargs)
-    
+
     if controlnet is not None:
         if isinstance(controlnet, list):
             controlnet = [load_controlnet(controlnet_path, torch_dtype=torch_dtype) for controlnet_path in controlnet]
         else:
             controlnet = load_controlnet(controlnet, torch_dtype=torch_dtype)
         model_kwargs.update(controlnet=controlnet)
-    
+
     if pipeline_class is None:
         if controlnet is not None:
             pipeline_class = StableDiffusionControlNetPipeline
         else:
             pipeline_class = StableDiffusionPipeline
-    
+
     pipe: StableDiffusionPipeline = model_from_ckpt_or_pretrained(
         base_model,
         pipeline_class,
@@ -105,7 +120,7 @@ def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controln
         pipe.set_ip_adapter_scale(1.0)
     else:
         pipe.unload_ip_adapter()
-    
+
     pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
     if model_cpu_offload_seq is None:
@@ -115,7 +130,7 @@ def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controln
             pipe.model_cpu_offload_seq = "text_encoder->controlnet->vae->unet->vae"
     else:
         pipe.model_cpu_offload_seq = model_cpu_offload_seq
-    
+
     if enable_sequential_cpu_offload:
         pipe.enable_sequential_cpu_offload()
     else:
@@ -124,8 +139,7 @@ def load_common_sd15_pipe(base_model=DEFAULT_BASE_MODEL, device="auto", controln
         # pipe.enable_model_cpu_offload()
     if vae_slicing:
         pipe.enable_vae_slicing()
-        
+
     import gc
     gc.collect()
     return pipe
-
